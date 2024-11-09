62b90e2f6aba8d26b705410c79d120f4494eaf51
MostafaSensei106 <138288138+MostafaSensei106@users.noreply.github.com>
2024-11-09 18:43:47 +0200

    SetUp Project

---
+++ lib/tadamon.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tadamon_app/core/config/theme/colors/dark_theme.dart';
+import 'package:tadamon_app/core/config/theme/colors/light_theme.dart';
+import 'package:tadamon_app/core/routing/app_router.dart';
+
+class TadamonApp extends StatelessWidget {
+  final AppRouter appRouter = AppRouter();
+  TadamonApp(AppRouter appRouter, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'تضامن',
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: ThemeMode.system,
+        initialRoute: 'Routes.onBoardingScreen',
+        onGenerateRoute: appRouter.generateRoute,
+        locale: const Locale('ar'),
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate
+        ],
+        supportedLocales: const [
+          Locale('ar'),
+        ],
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tadamon_app/main.dart';
+import 'package:tadamon_app/core/routing/app_router.dart';
+import 'package:tadamon_app/tadamon.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(TadamonApp(AppRouter()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

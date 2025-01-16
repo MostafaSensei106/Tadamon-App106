19684fd398b6c8f996a1a6bb59914d68030510f6
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-01-16 02:20:52 +0200

    Changes to be committed: 	modified:   devtools_options.yaml 	modified:   lib/core/widget/text_filed_component/text_filed_component.dart 	modified:   lib/features/pages/home_page/ui/page/home_page.dart 	new file:   lib/features/report_products/logic/bloc/report_product_cubit.dart 	new file:   lib/features/report_products/logic/bloc/report_product_state.dart 	modified:   lib/features/report_products/logic/services/report_service.dart 	modified:   lib/features/report_products/widgets/report_products_seet_content/radio_selection_tile_component.dart 	modified:   lib/features/report_products/widgets/report_products_seet_content/report_product_sheet_content.dart

--- lib/features/report_products/logic/services/report_service.dart
+++ lib/features/report_products/logic/services/report_service.dart
@@ -3,27 +3,27 @@ import 'package:tadamon/features/app_toast/app_toast.dart';
 import 'package:tadamon/features/barcode_scanner/data/repo/fire_store_services.dart';
 
 class ReportService {
-/// Sends a product report to the backend if there is an internet connection.
-/// 
-/// The function takes a [productReport] map containing details of the product report.
-/// It checks the network connection before attempting to send the report.
-/// If the report is successfully sent, a success toast message is shown.
-/// If there is an error during the sending process, an error toast message is displayed.
-/// If there is no internet connection, a toast message informs the user that the report
-/// will be sent automatically once the connection is restored.
+  /// Sends a product report to the backend if there is an internet connection.
+  ///
+  /// The function takes a [productReport] map containing details of the product report.
+  /// It checks the network connection before attempting to send the report.
+  /// If the report is successfully sent, a success toast message is shown.
+  /// If there is an error during the sending process, an error toast message is displayed.
+  /// If there is no internet connection, a toast message informs the user that the report
+  /// will be sent automatically once the connection is restored.
 
-  Future<void> sendProductReport(Map<String, dynamic> productReport) async {
+  static Future<void> sendProductReport(
+      Map<String, dynamic> productReport) async {
     if (await NetworkController().checkConnection() == true) {
       try {
-      await FireStoreServices().sendReportToBackEnd(productReport);
-      AppToast.showSuccessToast('لقد تلقينا بلاغك');
-        }catch(e){
-          AppToast.showErrorToast('حث خطاء');
-        }
-    }else{
-      AppToast.showToast('لا يوجد انترنت ولكن عند توافر سوف يتم ارسال بلاغك تلقائيا');
+        await FireStoreServices().sendReportToBackEnd(productReport);
+        AppToast.showSuccessToast('لقد تلقينا بلاغك');
+      } catch (e) {
+        AppToast.showErrorToast('حث خطاء');
+      }
+    } else {
+      AppToast.showToast(
+          'لا يوجد انترنت ولكن عند توافر سوف يتم ارسال بلاغك تلقائيا');
     }
   }
-
 }
-
--- lib/features/report_products/widgets/report_products_seet_content/report_product_sheet_content.dart
+++ lib/features/report_products/widgets/report_products_seet_content/report_product_sheet_content.dart
@@ -1,12 +1,11 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tadamon/core/config/const/sensei_const.dart';
 import 'package:tadamon/core/widget/button_component/button_compnent.dart';
 import 'package:tadamon/core/widget/text_filed_component/text_filed_component.dart';
-import 'package:tadamon/features/app_toast/app_toast.dart';
-import 'package:tadamon/features/barcode_scanner/logic/logic/scanner_manger.dart';
-import 'package:tadamon/features/report_products/logic/services/report_service.dart';
+import 'package:tadamon/features/report_products/logic/bloc/report_product_cubit.dart';
+import 'package:tadamon/features/report_products/logic/bloc/report_product_state.dart';
 import 'package:tadamon/features/report_products/widgets/report_products_seet_content/radio_selection_tile_component.dart';
 
 class ReportProductSheetContent extends StatefulWidget {
@@ -18,157 +17,121 @@ class ReportProductSheetContent extends StatefulWidget {
 }
 
 class _ReportProductSheetContentState extends State<ReportProductSheetContent> {
-  final serialNumberController = TextEditingController();
-
-  final productNameController = TextEditingController();
-
+  final TextEditingController serialNumberController = TextEditingController();
+  final TextEditingController productNameController = TextEditingController();
   String status = 'لا أعرف';
 
-  bool isFormValid = false;
-
   @override
-  void initState() {
-    super.initState();
-    serialNumberController.addListener(() {
-      _validateForm();
-    });
-    productNameController.addListener(() {
-      _validateForm();
-    });
-  }
-
-  /// Validates the form by checking if both the serial number and product name
-  /// controllers are not empty. If both are not empty, the [isFormValid] state
-  /// variable is set to true. Otherwise, it is set to false. The state is
-  /// updated using [setState] to trigger a rebuild of the widget tree when the
-  /// form is validated.
-  void _validateForm() {
-    setState(() {
-      isFormValid = serialNumberController.text.isNotEmpty &&
-          productNameController.text.isNotEmpty;
-    });
-  }
-
-  /// Scans the barcode using the device's camera and sets the scanned serial number
-  /// to the [serialNumberController].
+  /// A [StatefulWidget] that builds a sheet for reporting a product.
   ///
-  /// If the scan is successful, the scanned serial number is set to the
-  /// [serialNumberController]. If the scan failed or was cancelled, the method
-  /// returns without doing anything.
+  /// The sheet contains a [TextFieldComponent] for entering the serial number
+  /// of the product, another [TextFieldComponent] for entering the product name,
+  /// a [RadioSelectionTileComponent] for selecting the status of the product, and
+  /// a [ButtonCompnent] for submitting the report.
   ///
-  /// If an error occurred while scanning the barcode, an error toast is displayed
-  /// with the error message, and the method returns without doing anything.
-  Future<void> _scanBarcode(BuildContext context) async {
-    try {
-      String? resSerialNumber = await ScannerManager().scanBarcode(context);
-      if (!context.mounted ||
-          resSerialNumber == null ||
-          resSerialNumber == '-1') {
-        return;
-      }
-      serialNumberController.text = resSerialNumber;
-    } catch (e) {
-      AppToast.showErrorToast(e.toString());
-    }
-  }
-
-  /// Submits the report to the FireStore database and pops the bottom sheet.
+  /// The sheet uses a [BlocProvider] to provide the [ReportProductCubit] to its
+  /// children. The [BlocBuilder] is used to listen to the state of the
+  /// [ReportProductCubit] and rebuild the sheet when the state changes.
   ///
-  /// The report is constructed from the [serialNumberController] and
-  /// [productNameController] and the selected [status]. The timestamp of the
-  /// report is set to the current time using [Timestamp.now].
+  /// The validity of the report is determined by the [ReportProductIsValid] state
+  /// of the [ReportProductCubit]. If the report is valid, the button is enabled.
+  /// If the report is invalid, the button is disabled and an error message is
+  /// displayed.
   ///
-  /// If the report is submitted successfully, the bottom sheet is popped. If an
-  /// error occurs, an error toast is displayed with the error message.
-  void submitReport(BuildContext context) async {
-    final report = {
-      'serialNumber': serialNumberController.text,
-      'productName': productNameController.text,
-      'status': status,
-      'timestamp': Timestamp.now(),
-    };
-
-    try {
-      Navigator.pop(context);
-      await ReportService().sendProductReport(report);
-    } catch (e) {
-      AppToast.showErrorToast('حدث خطأ أثناء إرسال التقرير: $e');
-    }
-  }
-
-  @override
-  /// Builds a bottom sheet content with a form to report a product.
+  /// If the report is submitted successfully, the sheet is popped from the
+  /// navigator.
   ///
-  /// The form has two text fields: the first is for the serial number of the
-  /// product, and the second is for the product name. The serial number field
-  /// has a numeric keyboard and a button to scan the barcode of the product.
-  /// The product name field has a label keyboard.
-  ///
-  /// The form also has a radio selection tile with two options: 'مقاطعة' and
-  /// 'لا يدعم الكيان'. The selected value is stored in the [status] variable.
-  ///
-  /// The form is validated by checking if both the serial number and product name
-  /// fields are not empty. If both are not empty, the [isFormValid] state
-  /// variable is set to true. Otherwise, it is set to false. The state is
-  /// updated using [setState] to trigger a rebuild of the widget tree when the
-  /// form is validated.
-  ///
-  /// The form also has a button to submit the report. The button is disabled if
-  /// the form is not valid. When the button is pressed, the [submitReport] method
-  /// is called with the [context] as an argument.
+  /// If an error occurs while submitting the report, an error message is
+  /// displayed.
   Widget build(BuildContext context) {
-    return Column(children: [
-      TextFiledComponent(
-        controller: serialNumberController,
-        icon: Icons.qr_code_rounded,
-        isNumeric: true,
-        hint: 'ادخل رقم التسلسلي',
-        errorText: 'الرقم التسلسلي مطلوب',
-        suffixIcon: IconButton(
-          icon: const Icon(Icons.camera_alt_outlined),
-          onPressed: () => _scanBarcode(context),
-        ),
-      ),
-      SizedBox(
-        height: SenseiConst.margin.h,
-      ),
-      TextFiledComponent(
-        controller: productNameController,
-        icon: Icons.label_outline_rounded,
-        hint: 'ادخل اسم المنتج',
-        errorText: 'اسم المنتج مطلوب',
-      ),
-      SizedBox(
-        height: SenseiConst.margin.h,
-      ),
-      RadioSelectionTileComponent(
-        onChanged: (value) {
-          status = value;
+    return BlocProvider(
+      create: (_) => ReportProductCubit(),
+      child: BlocBuilder<ReportProductCubit, ReportProductState>(
+        builder: (context, state) {
+          
+          final bool isValid = state is ReportProductIsValid;
+          bool isSerialNumberError = state is ReportProductSerialNumberIsNotValid;
+          bool isProductNameError = state is ReportProductProductNameIsNotValid;
+
+/// Submits the product report using the [ReportProductCubit] and closes the sheet.
+/// 
+/// If both the product name and serial number are invalid, the report is not submitted.
+/// Upon successful submission, the sheet is popped from the navigation stack.
+
+
+          void sendReport(BuildContext context) {
+            if (state is ReportProductProductNameIsNotValid &&
+                state is ReportProductSerialNumberIsNotValid) {
+              return;
+            }
+            context.read<ReportProductCubit>().submitReport(
+                serialNumberController.text,
+                productNameController.text,
+                status);
+            Navigator.pop(context);
+          }
+
+          return Column(
+            children: [
+              TextFieldComponent(
+                controller: serialNumberController,
+                onChange: (_) => context
+                    .read<ReportProductCubit>()
+                    .validateInputs(serialNumberController.text,
+                        productNameController.text),
+                icon: Icons.qr_code_rounded,
+                isNumeric: true,
+                hint: 'ادخل الرقم التسلسلي (6-13 أرقام)',
+                errorText: isSerialNumberError ? state.error : null,
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.camera_alt_outlined),
+                  onPressed: () => context
+                      .read<ReportProductCubit>()
+                      .scanBarcode(context, serialNumberController),
+                ),
+              ),
+              SizedBox(
+                height: SenseiConst.margin.h,
+              ),
+              TextFieldComponent(
+                controller: productNameController,
+                onChange: (_) => context
+                    .read<ReportProductCubit>()
+                    .validateInputs(serialNumberController.text,
+                        productNameController.text),
+                icon: Icons.label_outline_rounded,
+                hint: 'ادخل اسم المنتج',
+                errorText: isProductNameError ? state.error : null,
+              ),
+              SizedBox(
+                height: SenseiConst.margin.h,
+              ),
+              RadioSelectionTileComponent(
+                onChanged: (value) {
+                  setState(() {
+                    status = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: SenseiConst.margin.h,
+              ),
+              if (state is ReportProductIsLoading)
+                const CircularProgressIndicator(),
+              ButtonCompnent(
+                label: 'ارسال التقرير',
+                icon: Icons.send,
+                onPressed: isValid ? () => {sendReport(context)} : null,
+              ),
+              if (state is ReportProductIsError)
+                Text(
+                  state.error,
+                  style: const TextStyle(color: Colors.red),
+                )
+            ],
+          );
         },
       ),
-      SizedBox(
-        height: SenseiConst.margin.h,
-      ),
-      ButtonCompnent(
-          label: 'ارسال بالتقرير',
-          icon: Icons.send,
-          onPressed: isFormValid ? () => submitReport(context) : null),
-    ]);
-  }
-
-
-  @override
-  /// Disposes of the text controllers and calls [super.dispose].
-  ///
-  /// This is called when the widget is removed from the tree.
-  ///
-  /// It is important to call [dispose] to free up resources when the widget
-  /// is no longer needed.
-  void dispose() {
-    serialNumberController.removeListener(() {});
-    productNameController.removeListener(() {});
-    serialNumberController.dispose();
-    productNameController.dispose();
-    super.dispose();
+    );
   }
 }

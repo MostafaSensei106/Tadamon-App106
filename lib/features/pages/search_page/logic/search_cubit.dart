0dd15596681a0d196e62df2db048bb4e77a603b3
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-01-19 16:09:33 +0200

    Changes to be committed: 	new file:   lib/features/pages/search_page/logic/search_cubit.dart 	new file:   lib/features/pages/search_page/logic/search_state.dart

---
+++ lib/features/pages/search_page/logic/search_cubit.dart
@@ -0,0 +1,9 @@
+
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tadamon/features/pages/search_page/logic/search_state.dart';
+
+class SearchCubit extends Cubit<SearchState> {
+  SearchCubit(super.initialState);
+  void search(String query) {}
+}
---
+++ lib/features/pages/search_page/logic/search_state.dart
@@ -0,0 +1,12 @@
+
+abstract class SearchState {
+
+}
+
+class SearchStateInit extends SearchState {}
+
+class SearchStateLoading extends SearchState {}
+
+class SearchStateLoadingSuccess extends SearchState {}
+
+class SearchStateLoadingError extends SearchState {}

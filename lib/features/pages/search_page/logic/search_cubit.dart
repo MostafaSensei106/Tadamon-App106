a563bb53d3118c5cf33a4e3626bb57f0c9934f01
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-01-28 22:57:34 +0200

    i do not know wtf i did in this app

--- lib/features/pages/search_page/logic/search_cubit.dart
+++ lib/features/pages/search_page/logic/search_cubit.dart
@@ -1,9 +1,21 @@
-
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tadamon/features/pages/search_page/logic/search_state.dart';
+import 'package:tadamon/features/products_scanner/data/repository/fire_store_services.dart';
 
 class SearchCubit extends Cubit<SearchState> {
   SearchCubit(super.initialState);
-  void search(String query) {}
+
+  Future<void> search(String query) async {
+    emit(SearchStateLoading());
+
+    try {
+      emit(SearchStateLoadingSuccess(
+        products: await FireStoreServices().search(query),
+      ));
+    } catch (e) {
+      emit(SearchStateLoadingError());
+    }
+
+  }
+
 }

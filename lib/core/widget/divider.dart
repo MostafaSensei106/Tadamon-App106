3c83a9619bb54d1b59582c430fed15e0af517312
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-02-15 11:44:59 +0200

    Changes to be committed: 	modified:   android/app/.cxx/Debug/134lo675/arm64-v8a/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/armeabi-v7a/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/x86/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/x86_64/configure_fingerprint.bin 	modified:   lib/core/widget/divider.dart 	modified:   lib/features/pages/search_page/ui/widget/product_expansion_tile.dart

--- lib/core/widget/divider.dart
+++ lib/core/widget/divider.dart
@@ -8,6 +8,7 @@ class SenseiDivider extends StatelessWidget {
   /// Returns a [Divider] widget with a thickness, height, indent, and endIndent of
   /// 0, and a color of [Theme.of(context).colorScheme.outline] with an opacity
   /// of 0.8.
+  
   Widget build(BuildContext context) {
     return Divider(
       thickness: 0.1,
@@ -17,4 +18,5 @@ class SenseiDivider extends StatelessWidget {
       color: Theme.of(context).colorScheme.outline.withAlpha(0x80),
     );
   }
+  
 }

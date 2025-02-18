7c1a54b093af56940713921cee743501ad0a5fd1
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-02-18 20:08:56 +0200

    Changes to be committed: 	modified:   lib/features/pages/onboarding_page/ui/page/onboarding_page.dart 	modified:   lib/features/pages/onboarding_page/ui/widget/dot_indicator_nav.dart 	modified:   lib/features/pages/onboarding_page/ui/widget/onboarding_page_one.dart 	modified:   lib/features/pages/onboarding_page/ui/widget/onboarding_page_two.dart

--- lib/features/pages/onboarding_page/ui/widget/onboarding_page_two.dart
+++ lib/features/pages/onboarding_page/ui/widget/onboarding_page_two.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -14,6 +15,7 @@ class OnboardingPageTwo extends StatefulWidget {
   final String secondTitle;
   final String subtitle;
   final bool appOnlineRun;
+  final double height;
 
   const OnboardingPageTwo({
     super.key,
@@ -23,6 +25,7 @@ class OnboardingPageTwo extends StatefulWidget {
     required this.secondTitle,
     required this.subtitle,
     this.appOnlineRun = false,
+     this.height = 0.0,
   });
 
   @override
@@ -56,20 +59,21 @@ class _OnboardingPageTwoState extends State<OnboardingPageTwo> {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
+        SizedBox(height: widget.height.h),
         Container(
           padding: EdgeInsets.all(SenseiConst.padding.w),
-          width: 200,
-          height: 200,
           decoration: BoxDecoration(
             color: Theme.of(context).colorScheme.surfaceContainer,
-            borderRadius:
-                BorderRadius.circular(SenseiConst.outBorderRadius.r),
+            borderRadius: BorderRadius.circular(SenseiConst.outBorderRadius.r),
             border: Border.all(
               color: Theme.of(context).colorScheme.outline.withAlpha(0x80),
             ),
           ),
           child: Container(
+            width: 200.w,
+            height: 200.h,
             decoration: BoxDecoration(
                 borderRadius:
                     BorderRadius.circular(SenseiConst.inBorderRadius.r),
@@ -87,55 +91,58 @@ class _OnboardingPageTwoState extends State<OnboardingPageTwo> {
             ),
           ),
         ),
-        SizedBox(height: 25.h),
-        AnimatedCrossFade(
-          firstChild: Text(
-            widget.firstTitle,
-            style: TextStyle(
-              fontSize: 28.sp,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          ),
-          secondChild: Text(
-            widget.secondTitle,
-            style: TextStyle(
-              fontSize: 28.sp,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          ),
-          crossFadeState: isFirstIcon
-              ? CrossFadeState.showFirst
-              : CrossFadeState.showSecond,
-          duration: const Duration(milliseconds: 350),
-        ),
         SizedBox(height: 10.h),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30.w),
-          child: Text(
-            widget.subtitle,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 16.sp,
-              color: Theme.of(context).colorScheme.onSurface.withAlpha(0x80),
+        Column(
+          children: [
+            AnimatedCrossFade(
+              firstChild: Text(
+                widget.firstTitle,
+                style: TextStyle(
+                  fontSize: 28.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              secondChild: Text(
+                widget.secondTitle,
+                style: TextStyle(
+                  fontSize: 28.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              crossFadeState: isFirstIcon
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: const Duration(milliseconds: 350),
             ),
-          ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.w),
+              child: Text(
+                widget.subtitle,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  color:
+                      Theme.of(context).colorScheme.onSurface.withAlpha(0x80),
+                ),
+              ),
+            ),
+            if (widget.appOnlineRun) ...[
+              ButtonCompnent(
+                useInBorderRadius: false,
+                useWidth: true,
+                width: 0.5.sw,
+                label: 'تشغيل الاونلاين',
+                icon: Icons.cloud_download_outlined,
+                onPressed: () {
+                  HapticFeedback.vibrate();
+                  // context.read<HiveCubit>().fetchDataFromFireStore();
+                },
+              ),
+            ]
+          ],
         ),
-        if (widget.appOnlineRun) ...[
-          ButtonCompnent(
-            useMargin: true,
-            useInBorderRadius: false,
-            useWidth: true,
-            width: 0.5.sw,
-            label: 'تشغيل الاونلاين',
-            icon: Icons.cloud_download_outlined,
-            onPressed: () {
-              HapticFeedback.vibrate();
-              // context.read<HiveCubit>().fetchDataFromFireStore();
-            },
-          ),
-        ]
       ],
     );
   }

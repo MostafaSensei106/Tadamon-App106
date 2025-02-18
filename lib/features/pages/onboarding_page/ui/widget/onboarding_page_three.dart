1d4f675eda027682e9f7c062e8428cc594a5b948
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-02-18 18:16:48 +0200

    Changes to be committed: 	modified:   android/app/.cxx/Debug/134lo675/arm64-v8a/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/armeabi-v7a/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/x86/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/x86_64/configure_fingerprint.bin 	modified:   lib/core/config/const/sensei_const.dart 	modified:   lib/features/pages/onboarding_page/ui/page/onboarding_page.dart 	new file:   lib/features/pages/onboarding_page/ui/widget/animated_triangles.dart 	new file:   lib/features/pages/onboarding_page/ui/widget/dot_indicator_nav.dart 	deleted:    lib/features/pages/onboarding_page/ui/widget/onboarding_page_intro.dart 	new file:   lib/features/pages/onboarding_page/ui/widget/onboarding_page_one.dart 	new file:   lib/features/pages/onboarding_page/ui/widget/onboarding_page_three.dart 	new file:   lib/features/pages/onboarding_page/ui/widget/onboarding_page_two.dart

---
+++ lib/features/pages/onboarding_page/ui/widget/onboarding_page_three.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPageThree extends StatelessWidget {
+  const OnboardingPageThree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

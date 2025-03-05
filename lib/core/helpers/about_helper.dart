a59ca3fab4df3c876e67915641dfa4fdfc3a27a2
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-03-05 17:40:08 +0200

    Changes to be committed: 	renamed:    lib/core/config/theme/colors/dark_theme.dart -> lib/core/config/theme/fonts/colors/dark_theme.dart 	renamed:    lib/core/config/theme/colors/light_theme.dart -> lib/core/config/theme/fonts/colors/light_theme.dart 	renamed:    lib/core/config/theme/logic/theme_cubit.dart -> lib/core/config/theme/fonts/colors/logic/theme_cubit.dart 	renamed:    lib/core/config/theme/logic/theme_shared_preferences.dart -> lib/core/config/theme/fonts/colors/logic/theme_shared_preferences.dart 	renamed:    lib/core/config/theme/logic/theme_state.dart -> lib/core/config/theme/fonts/colors/logic/theme_state.dart 	new file:   lib/core/helpers/about_helper.dart 	new file:   lib/core/helpers/dev_helper.dart 	new file:   lib/core/helpers/extensions/date.dart 	new file:   lib/core/helpers/extensions/navigetion_extensions.dart 	new file:   lib/core/helpers/font_weight_helper.dart 	new file:   lib/core/helpers/language_toggel_helper.dart 	new file:   lib/core/helpers/theme_toggle_helper.dart

---
+++ lib/core/helpers/about_helper.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tadamon/core/config/const/sensei_const.dart';
+import 'package:tadamon/core/widget/textbutton_component/textbutton_component.dart';
+import 'package:tadamon/generated/l10n.dart';
+
+void appAbout(BuildContext context) {
+  Navigator.pop(context);
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(SenseiConst.outBorderRadius.r),
+      ),
+      title: Row(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(SenseiConst.inBorderRadius.r),
+          clipBehavior: Clip.antiAlias,
+          child: Image.asset(
+            width: 45.w,
+            height: 45.w,
+            SenseiConst.tadamonAppImage,
+          ),
+        ),
+        SizedBox(
+          width: 10.w,
+        ),
+        Column(
+          children: [
+            Text(
+              'About',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const Text(
+              'v${SenseiConst.appVersion}',
+            ),
+          ],
+        )
+      ]),
+      content: Container(
+        padding: EdgeInsets.all(SenseiConst.padding.w),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(SenseiConst.inBorderRadius.r),
+          color: Theme.of(context).colorScheme.surfaceContainer,
+        ),
+        child: Text(
+          'تم تطوير هذا ',
+          textAlign: TextAlign.justify,
+          style: TextStyle(
+            fontSize: 16.sp,
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        TextButtonComponent(
+          icon: Icons.close,
+          onpressed: () => Navigator.pop(context),
+          text: S.of(context).close,
+          isClose: true,
+        ),
+
+        ///show license
+        TextButtonComponent(
+          icon: Icons.difference_outlined,
+        
+          onpressed: () {
+            showLicensePage(
+              context: context,
+              applicationName: 'تطبيق تضامن',
+              applicationLegalese:
+                  'تم تطوير هذا التطبيق من اجل دعم القضية الفلسطينية',
+              applicationIcon: Container(
+                padding: EdgeInsets.all(SenseiConst.padding.w),
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surfaceContainer,
+                    borderRadius:
+                        BorderRadius.circular(SenseiConst.outBorderRadius.r),
+                    border: Border.all(
+                      color:
+                          Theme.of(context).colorScheme.outline.withAlpha(0x80),
+                    )),
+                child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.circular(SenseiConst.inBorderRadius.r),
+                    child: Image.asset(
+                        width: 100, height: 100, SenseiConst.tadamonAppImage)),
+              ),
+              applicationVersion: 'v${SenseiConst.appVersion}',
+              useRootNavigator: true,
+            );
+          },
+          text: 'الترخيص',
+        ),
+      ],
+    ),
+  );
+}

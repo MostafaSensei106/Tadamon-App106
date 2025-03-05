8a636f2588202144820e1ed34985fbce8350a90a
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-03-05 18:05:43 +0200

    Changes to be committed: 	modified:   android/app/.cxx/Debug/134lo675/arm64-v8a/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/armeabi-v7a/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/x86/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/x86_64/configure_fingerprint.bin 	modified:   lib/core/helpers/extensions/date_format.dart 	modified:   lib/features/report_products/logic/bloc/report_product_cubit.dart 	modified:   lib/features/report_products/widgets/report_products_seet_content/report_product_sheet_content.dart

--- lib/core/helpers/extensions/date_format.dart
+++ lib/core/helpers/extensions/date_format.dart
@@ -2,7 +2,7 @@ import 'package:intl/intl.dart';
 
 extension DateFormatExtension on DateTime {
   String get formatted {
-    final DateFormat formatter = DateFormat('dd-MM-yyyy h:mm a', 'ar');
+    final DateFormat formatter = DateFormat('yyyy-MM-dd, h:mm a', 'en');
     return formatter.format(this);
   }
 }

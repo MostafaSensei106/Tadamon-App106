5932b6f2433128a26f165c54a5877b4e45b9be45
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-03-05 17:40:08 +0200

    Changes to be committed: 	renamed:    lib/core/config/theme/colors/dark_theme.dart -> lib/core/config/theme/fonts/colors/dark_theme.dart 	renamed:    lib/core/config/theme/colors/light_theme.dart -> lib/core/config/theme/fonts/colors/light_theme.dart 	renamed:    lib/core/config/theme/logic/theme_cubit.dart -> lib/core/config/theme/fonts/colors/logic/theme_cubit.dart 	renamed:    lib/core/config/theme/logic/theme_shared_preferences.dart -> lib/core/config/theme/fonts/colors/logic/theme_shared_preferences.dart 	renamed:    lib/core/config/theme/logic/theme_state.dart -> lib/core/config/theme/fonts/colors/logic/theme_state.dart 	new file:   lib/core/helpers/about_helper.dart 	new file:   lib/core/helpers/dev_helper.dart 	new file:   lib/core/helpers/extensions/date.dart 	new file:   lib/core/helpers/extensions/navigetion_extensions.dart 	new file:   lib/core/helpers/font_weight_helper.dart 	new file:   lib/core/helpers/language_toggel_helper.dart 	new file:   lib/core/helpers/theme_toggle_helper.dart

---
+++ lib/core/helpers/language_toggel_helper.dart
@@ -0,0 +1,56 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:tadamon_app/core/config/const/sensei_const.dart';
+//
+// String getLanguageName(Locale locale) {
+//   switch (locale.languageCode) {
+//     case 'en':
+//       return 'English';
+//     case 'ar':
+//       return 'العربية';
+//     case 'ja':
+//       return '日本語';
+//     default:
+//       return 'القطة المشمشية';
+//   }
+// }
+//
+// class LanguageDropdown extends StatelessWidget {
+//   final Locale currentLocale;
+//   final Function(Locale?) onChanged;
+//
+//   const LanguageDropdown(
+//       {super.key, required this.currentLocale, required this.onChanged});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return DropdownButton<Locale>(
+//       elevation: 0,
+//       borderRadius: BorderRadius.circular(SenseiConst.inBorderRadius.r),
+//       iconSize: 30.sp,
+//       icon: const Icon(Icons.arrow_drop_down_rounded),
+//       underline: Divider(),
+//       value: currentLocale,
+//       onChanged: onChanged,
+//       items: [
+//         _buildDropdownMenuItem(const Locale('en'), Icons.flag),
+//         _buildDropdownMenuItem(const Locale('ar'), Icons.public),
+//         _buildDropdownMenuItem(const Locale('ja'), Icons.language),
+//       ],
+//     );
+//   }
+//
+//   DropdownMenuItem<Locale> _buildDropdownMenuItem(
+//       Locale locale, IconData icon) {
+//     return DropdownMenuItem(
+//       value: locale,
+//       child: Row(
+//         children: [
+//           Icon(icon, size: 30.sp),
+//           SizedBox(width: 10.w),
+//           Text(getLanguageName(locale)),
+//         ],
+//       ),
+//     );
+//   }
+// }

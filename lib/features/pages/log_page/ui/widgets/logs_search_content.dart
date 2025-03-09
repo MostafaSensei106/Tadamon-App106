ab99796742ec6cedd8d5744807f50cbb3d9706e4
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-03-09 22:24:08 +0200

    Changes to be committed: 	modified:   android/app/.cxx/Debug/134lo675/arm64-v8a/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/armeabi-v7a/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/x86/configure_fingerprint.bin 	modified:   android/app/.cxx/Debug/134lo675/x86_64/configure_fingerprint.bin 	modified:   android/app/.cxx/RelWithDebInfo/61x3o1aq/arm64-v8a/configure_fingerprint.bin 	modified:   android/app/.cxx/RelWithDebInfo/61x3o1aq/armeabi-v7a/configure_fingerprint.bin 	modified:   android/app/.cxx/RelWithDebInfo/61x3o1aq/x86/configure_fingerprint.bin 	modified:   android/app/.cxx/RelWithDebInfo/61x3o1aq/x86_64/configure_fingerprint.bin 	modified:   lib/features/pages/log_page/ui/widgets/logs_page_view.dart 	modified:   lib/features/pages/log_page/ui/widgets/logs_search_content.dart 	modified:   lib/features/pages/log_page/ui/widgets/productlogs_expansiontile_component.dart

--- lib/features/pages/log_page/ui/widgets/logs_search_content.dart
+++ lib/features/pages/log_page/ui/widgets/logs_search_content.dart
@@ -6,28 +6,52 @@ import 'package:tadamon/core/widget/lottie_component/lottie_component.dart';
 import 'package:tadamon/features/pages/log_page/logic/bloc/logs_bloc.dart';
 import 'package:tadamon/features/pages/log_page/logic/bloc/logs_state.dart';
 import 'package:tadamon/features/pages/log_page/ui/widgets/productlogs_expansiontile_component.dart';
-
 class LogsSearchContent extends StatelessWidget {
+  final TextEditingController searchController;
+
   const LogsSearchContent({
     super.key,
-    required TextEditingController searchController,
+    required this.searchController,
   });
-
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: BlocBuilder<LogsBloc, LogsState>(
         builder: (context, state) {
-          if (state is LogsLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
+          // أضف منطق البحث هنا
+          if (searchController.text.isNotEmpty && state is LogsLoadingSuccess) {
+            final filteredProducts = state.products.where((product) {
+              return product.serialNumber.contains(searchController.text);
+            }).toList();
+
+            if (filteredProducts.isEmpty) {
+              return const LottieComponent(
+                lottiePath: SenseiConst.lottieNoFoundAnimation,
+                text: 'هذا المنتج غير موجود في السجلات',
+              );
+            }
+
+            return ListView.separated(
+              itemCount: filteredProducts.length,
+              itemBuilder: (context, index) {
+                final product = filteredProducts[index];
+                return ProductLogsExpansionTileComponent(product: product);
+              },
+              separatorBuilder: (context, index) =>
+                  SizedBox(height: SenseiConst.margin.h),
             );
+          }
+
+          // الحالة العادية عند عدم البحث
+          if (state is LogsLoading) {
+            return const Center(child: CircularProgressIndicator());
           } else if (state is LogsLoadingSuccess) {
             if (state.products.isEmpty) {
-              return  const LottieComponent(
-                      lottiePath: SenseiConst.lottieNoFoundAnimation,
-                      text: 'لم يتم العثور على المنتج في السجلات');
+              return const LottieComponent(
+                lottiePath: SenseiConst.lottieSearchAnimation,
+                text: 'نتائج البحث سوف تظهر هنا',
+              );
             }
             return ListView.separated(
               itemCount: state.products.length,
@@ -39,14 +63,14 @@ class LogsSearchContent extends StatelessWidget {
                   SizedBox(height: SenseiConst.margin.h),
             );
           } else if (state is LogsError) {
-            return Center(
-             child: Text('Error: ${state.message}'),
-            );
+            return Center(child: Text('Error: ${state.message}'));
           } else {
             return const Center(
-                child: LottieComponent(
-                    lottiePath: SenseiConst.lottieSearchAnimation,
-                    text: 'نتائج البحث سوف تظهر هنا'));
+              child: LottieComponent(
+                lottiePath: SenseiConst.lottieSearchAnimation,
+                text: 'نتائج البحث سوف تظهر هنا',
+              ),
+            );
           }
         },
       ),

b5e4c59a30f6b21fbaf1f3fa68d7be81f200a5f1
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-03-09 21:34:10 +0200

    Changes to be committed: 	modified:   lib/core/routing/routes.dart

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,4 +1,10 @@
-//Routes in MyApp
+/// Class [Routes] contains all app routes as constants.
+///
+/// * [mainPage] is the root route of the app.
+/// * [onBoarding] is the route of the onboarding page.
+/// * [palatineMap] is the route of the Palestine map page.
+/// * [userHelp] is the route of the user help page.
+/// * [noRoutes] is the route of the "no routes" page.
 class Routes {
   static const String mainPage = '/';
   static const String onBoarding = '/onBoarding';

59c8151f26dd687ebb9ca4806a8e5257a0d54c99
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-03-09 21:31:16 +0200

    Changes to be committed: 	modified:   lib/features/pages/help_user_page/logic/cubit/help_user_cubit.dart 	modified:   lib/features/pages/help_user_page/logic/cubit/help_user_state.dart 	modified:   lib/features/pages/help_user_page/ui/widget/help_user_page_view.dart 	modified:   lib/features/pages/main_page/ui/widget/app_bar/side_page_app_bar.dart 	modified:   lib/features/pages/palestine_map_page/ui/page/palestine_map_page.dart

--- lib/features/pages/main_page/ui/widget/app_bar/side_page_app_bar.dart
+++ lib/features/pages/main_page/ui/widget/app_bar/side_page_app_bar.dart
@@ -3,39 +3,49 @@ import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tadamon/core/config/const/sensei_const.dart';
 
-class SidePageAppBar extends StatelessWidget {
+class SidePageAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
-  const SidePageAppBar({super.key, required this.title});
+  const SidePageAppBar({super.key, required this.title,});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
 
   /// Navigate back in the app by calling [Navigator.pop] on the given [context], and
   /// trigger a haptic feedback with [HapticFeedback.vibrate].
-  void leave(BuildContext context) {
+  void leave(context) {
     HapticFeedback.vibrate();
     Navigator.pop(context);
   }
 
   @override
+
+  /// Returns an [AppBar] widget with a centered title and a leading widget that is a left
+  /// arrow icon. When the icon is tapped, it triggers the [leave] function to navigate back in
+  /// the app. The [AppBar] has no elevation and a white background.
   Widget build(BuildContext context) {
-    return SliverAppBar(
+    return AppBar(
       title: Text(title),
       centerTitle: true,
       elevation: 0,
-      backgroundColor: Colors.transparent,
-     // pinned: true,
+      forceMaterialTransparency: true,
       leading: _buildSidePageAppBarIcon(context, Icons.keyboard_double_arrow_right_rounded),
     );
   }
 
-  /// Returns a [Padding] widget containing a [Material] widget with an [InkWell] child.
-  ///
-  /// The [InkWell] widget has a rounded border and is wrapped in a transparent [Material] widget.
-  /// When tapped, it triggers the [leave] function to navigate back in the app.
-  ///
-  /// The [InkWell] contains a [Container] with a rounded border and a background color of
-  /// [Theme.of(context).colorScheme.surfaceContainer]. Inside the [Container] is an [Icon] widget
-  /// with the provided [icon] parameter, sized according to [SenseiConst.iconSize] and colored
-  /// using [Theme.of(context).colorScheme.onSurface].
-  Widget _buildSidePageAppBarIcon(BuildContext context, IconData icon) {
+
+  
+/// Returns a [Padding] widget containing a [Material] widget with an [InkWell] child.
+/// 
+/// The [InkWell] widget has a rounded border and is wrapped in a transparent [Material] widget.
+/// When tapped, it triggers the [leave] function to navigate back in the app.
+/// 
+/// The [InkWell] contains a [Container] with a rounded border and a background color of 
+/// [Theme.of(context).colorScheme.surfaceContainer]. Inside the [Container] is an [Icon] widget 
+/// with the provided [icon] parameter, sized according to [SenseiConst.iconSize] and colored 
+/// using [Theme.of(context).colorScheme.onSurface].
+
+  Widget _buildSidePageAppBarIcon(BuildContext context, IconData icon ) {
     return Padding(
       padding: const EdgeInsets.all(SenseiConst.padding),
       child: Material(
@@ -59,4 +69,3 @@ class SidePageAppBar extends StatelessWidget {
     );
   }
 }
-
--- lib/features/pages/palestine_map_page/ui/page/palestine_map_page.dart
+++ lib/features/pages/palestine_map_page/ui/page/palestine_map_page.dart
@@ -11,49 +11,41 @@ class PalestineMapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          /// ✅ استخدام `SidePageAppBar` داخل `slivers`
-          SidePageAppBar(title: S.of(context).PalatineMap),
-
-          /// ✅ لف `InteractiveViewer` داخل `SliverToBoxAdapter`
-          SliverToBoxAdapter(
-            child: InteractiveViewer(
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(SenseiConst.outBorderRadius.r),
-                  topRight: Radius.circular(SenseiConst.outBorderRadius.r),
-                ),
-                child: SvgPicture.asset(
-                  SenseiConst.palestineMap,
-                  fit: BoxFit.cover,
-                  placeholderBuilder: (context) => const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                  errorBuilder: (context, error, stackTrace) {
-                    return Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.error_outline,
-                            color: Colors.red,
-                            size: SenseiConst.iconSize,
-                          ),
-                          const SizedBox(height: 16),
-                          Text(
-                            'حدث خطأ في تحميل الصورة',
-                            style: Theme.of(context).textTheme.bodyLarge,
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
+      appBar: SidePageAppBar(
+        title: S.of(context).PalatineMap,
+      ),
+      body: ClipRRect(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(SenseiConst.outBorderRadius.r+7),
+            topRight: Radius.circular(SenseiConst.outBorderRadius.r+7)),
+        child: InteractiveViewer(
+          child: SvgPicture.asset(
+            SenseiConst.palestineMap,
+            fit: BoxFit.cover,
+            placeholderBuilder: (context) => const Center(
+              child: CircularProgressIndicator(),
             ),
+            errorBuilder: (context, error, stackTrace) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.error_outline,
+                      color: Colors.red,
+                      size: SenseiConst.iconSize,
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      'حدث خطأ في تحميل الصورة',
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
-        ],
+        ),
       ),
     );
   }

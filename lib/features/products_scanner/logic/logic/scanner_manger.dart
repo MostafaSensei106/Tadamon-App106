f8954581b05fe00d08c7414e8db991e1afc1ca13
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-03-15 13:15:58 +0200

    Changes to be committed: 	modified:   lib/features/products_scanner/logic/logic/product_scan_bloc/product_scan_cubit.dart 	modified:   lib/features/products_scanner/logic/logic/scanner_manger.dart 	modified:   lib/features/report_products/logic/bloc/report_product_cubit.dart

--- lib/features/products_scanner/logic/logic/scanner_manger.dart
+++ lib/features/products_scanner/logic/logic/scanner_manger.dart
@@ -6,49 +6,47 @@ import 'package:tadamon/generated/l10n.dart';
 import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
 
 class ScannerManager {
-
-
-  //if barcode is number 
-  bool isNumber(String input) {
-    if (input.isEmpty) {
-      return false;
-    }
-    final number = num.tryParse(input);
-    return number != null;
+  bool isNumber(String barcode) {
+    final RegExp regExp = RegExp(r'^\d+$');
+    return regExp.hasMatch(barcode);
   }
-
 
   Future<String> scanBarcode(BuildContext context) async {
     try {
-      final String? barcodeScanRes = await SimpleBarcodeScanner.scanBarcode(
+      String? barcodeScanRes = await SimpleBarcodeScanner.scanBarcode(
         context,
-       barcodeAppBar: BarcodeAppBar(
-         appBarTitle: S.of(context).scanBarcode,
-         centerTitle: true,
-         enableBackButton: true,
-         backButtonIcon:const Icon(Icons.keyboard_double_arrow_right_rounded),
-       ),
+        barcodeAppBar: BarcodeAppBar(
+          appBarTitle: S.of(context).scanBarcode,
+          centerTitle: true,
+          enableBackButton: true,
+          backButtonIcon: const Icon(Icons.keyboard_double_arrow_right_rounded),
+        ),
         isShowFlashIcon: true,
         delayMillis: 0,
         cameraFace: CameraFace.back,
         scanFormat: ScanFormat.ONLY_BARCODE,
         scanType: ScanType.barcode,
-        // ignore: deprecated_member_use
-        lineColor: '#${Colors.yellow.value.toRadixString(16).padLeft(8, '0').toUpperCase()}',
+        lineColor: '#FF0000',
         cancelButtonText: S.of(context).close,
       );
-      if (barcodeScanRes == null || barcodeScanRes == '-1'){
+      if (barcodeScanRes == null || barcodeScanRes == '-1') {
         AppToast.showErrorToast('لم يتم قراءة اي باركود');
         return '-1';
       }
+
+      if (!isNumber(barcodeScanRes)) {
+        AppToast.showErrorToast(
+            'الباركود :$barcodeScanRes غير صالح، يجب أن يكون رقمًا فقط');
+        return '-404';
+      }
+
       return barcodeScanRes;
     } catch (e) {
-      AppToast.showErrorToast('Error occurred while scanning the barcode: ${e.toString()}');
-      return '-999';
+      'حدث خطأ أثناء قراءة الباركود: ${e.toString()}';
+      return '-404';
     }
   }
 
-  
   Future<String?> imageAnalysisScan(BuildContext context) async {
     try {
       final XFile? image = await _pickImage();
@@ -58,20 +56,20 @@ class ScannerManager {
       }
       final String? barcode = await _getBarcodeFromImage(image);
       if (barcode == null) {
-        AppToast.showErrorToast('الصورة غير واضحة او لا تحتوي علي باركود');
+        AppToast.showErrorToast('الصورة غير واضحة أو لا تحتوي على باركود ');
+        return null;
+      }
+      if (!isNumber(barcode)) {
+        AppToast.showErrorToast(
+            'الباركود :$barcode غير صالح، يجب أن يكون رقمًا فقط');
         return null;
       }
       return barcode;
     } catch (e) {
-      AppToast.showErrorToast('Error occurred while scanning the image: ${e.toString()}');
+      AppToast.showErrorToast('حدث خطأ أثناء تحليل الصورة: ${e.toString()}');
       return null;
     }
   }
-
-  
-  
-
-
 
   Future<XFile?> _pickImage() async {
     try {
@@ -82,26 +80,23 @@ class ScannerManager {
       );
       return image;
     } catch (e) {
-      AppToast.showErrorToast('Error occurred while picking the image: ${e.toString()}');
+      AppToast.showErrorToast('حدث خطأ أثناء اختيار الصورة: ${e.toString()}');
       return null;
     }
   }
-
-
-
-
 
   Future<String?> _getBarcodeFromImage(XFile image) async {
     try {
       final InputImage inputImage = InputImage.fromFilePath(image.path);
       final BarcodeScanner barcodeScanner = BarcodeScanner();
-      final List<Barcode> barcodes = await barcodeScanner.processImage(inputImage);
+      final List<Barcode> barcodes =
+          await barcodeScanner.processImage(inputImage);
       if (barcodes.isEmpty) {
         return null;
       }
       return barcodes.first.rawValue;
     } catch (e) {
-      AppToast.showErrorToast('Error occurred while processing the image: ${e.toString()}');
+      AppToast.showErrorToast('حدث خطأ أثناء معالجة الصورة: ${e.toString()}');
       return null;
     }
   }

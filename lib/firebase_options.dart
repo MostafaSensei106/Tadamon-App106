89f57ad316049752a6b86d5dc49c7c6389e304f9
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-03-18 22:17:10 +0200

    Changes to be committed: 	modified:   .gitignore 	new file:   lib/firebase_options.dart 	modified:   lib/main.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml
    Changes not staged for commit:
    	deleted:    android/app/.cxx/Debug/134lo675/arm64-v8a/.cmake/api/v1/reply/index-2025-03-15T10-12-52-0606.json
    	modified:   android/app/.cxx/Debug/134lo675/arm64-v8a/android_gradle_build.json
    	modified:   android/app/.cxx/Debug/134lo675/arm64-v8a/android_gradle_build_mini.json
    	modified:   android/app/.cxx/Debug/134lo675/arm64-v8a/configure_fingerprint.bin
    	modified:   android/app/.cxx/Debug/134lo675/arm64-v8a/metadata_generation_command.txt
    	modified:   android/app/.cxx/Debug/134lo675/arm64-v8a/prefab_config.json
    	deleted:    android/app/.cxx/Debug/134lo675/armeabi-v7a/.cmake/api/v1/reply/index-2025-03-15T10-12-55-0482.json
    	modified:   android/app/.cxx/Debug/134lo675/armeabi-v7a/android_gradle_build.json
    	modified:   android/app/.cxx/Debug/134lo675/armeabi-v7a/android_gradle_build_mini.json
    	modified:   android/app/.cxx/Debug/134lo675/armeabi-v7a/configure_fingerprint.bin
    	modified:   android/app/.cxx/Debug/134lo675/armeabi-v7a/metadata_generation_command.txt
    	modified:   android/app/.cxx/Debug/134lo675/armeabi-v7a/prefab_config.json
    	modified:   android/app/.cxx/Debug/134lo675/hash_key.txt
    	deleted:    android/app/.cxx/Debug/134lo675/x86/.cmake/api/v1/reply/index-2025-03-15T10-12-58-0025.json
    	modified:   android/app/.cxx/Debug/134lo675/x86/android_gradle_build.json
    	modified:   android/app/.cxx/Debug/134lo675/x86/android_gradle_build_mini.json
    	modified:   android/app/.cxx/Debug/134lo675/x86/configure_fingerprint.bin
    	modified:   android/app/.cxx/Debug/134lo675/x86/metadata_generation_command.txt
    	modified:   android/app/.cxx/Debug/134lo675/x86/prefab_config.json
    	deleted:    android/app/.cxx/Debug/134lo675/x86_64/.cmake/api/v1/reply/index-2025-03-15T10-12-59-0066.json
    	modified:   android/app/.cxx/Debug/134lo675/x86_64/android_gradle_build.json
    	modified:   android/app/.cxx/Debug/134lo675/x86_64/android_gradle_build_mini.json
    	modified:   android/app/.cxx/Debug/134lo675/x86_64/configure_fingerprint.bin
    	modified:   android/app/.cxx/Debug/134lo675/x86_64/metadata_generation_command.txt
    	modified:   android/app/.cxx/Debug/134lo675/x86_64/prefab_config.json
    	modified:   android/app/.cxx/RelWithDebInfo/61x3o1aq/arm64-v8a/configure_fingerprint.bin
    	modified:   android/app/.cxx/RelWithDebInfo/61x3o1aq/armeabi-v7a/configure_fingerprint.bin
    	modified:   pubspec.yaml
    
    Untracked files:
    	android/app/.cxx/Debug/134lo675/arm64-v8a/.cmake/api/v1/reply/index-2025-03-18T20-03-06-0190.json
    	android/app/.cxx/Debug/134lo675/armeabi-v7a/.cmake/api/v1/reply/index-2025-03-18T20-03-08-0861.json
    	android/app/.cxx/Debug/134lo675/x86/.cmake/api/v1/reply/index-2025-03-18T20-03-11-0027.json
    	android/app/.cxx/Debug/134lo675/x86_64/.cmake/api/v1/reply/index-2025-03-18T20-03-12-0078.json

---
+++ lib/firebase_options.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      default:
+        throw UnsupportedError('DefaultFirebaseOptions are not supported for this platform.');
+    }
+  }
+
+  static FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.env['API_KEY_ANDROID'] ?? '',
+    appId: dotenv.env['APP_ID_ANDROID'] ?? '',
+    messagingSenderId: dotenv.env['MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['PROJECT_ID'] ?? '',
+    databaseURL: dotenv.env['DATABASE_URL'] ?? '',
+    storageBucket: dotenv.env['STORAGE_BUCKET'] ?? '',
+  );
+
+  static FirebaseOptions ios = FirebaseOptions(
+    apiKey: dotenv.env['API_KEY_IOS'] ?? '',
+    appId: dotenv.env['APP_ID_IOS'] ?? '',
+    messagingSenderId: dotenv.env['MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['PROJECT_ID'] ?? '',
+    databaseURL: dotenv.env['DATABASE_URL'] ?? '',
+    storageBucket: dotenv.env['STORAGE_BUCKET'] ?? '',
+    iosBundleId: dotenv.env['IOS_BUNDLE_ID'] ?? '',
+  );
+}

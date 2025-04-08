f8033ef2d884c5a8bd6d391dc9908fcde4bfa2fc
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2025-04-08 14:59:13 +0200

    Changes to be committed: 	modified:   lib/core/routing/app_router.dart 	modified:   lib/core/services/url_services/url_services.dart 	modified:   lib/features/pages/chat_with_dev/ui/page/chat_with_dev.dart

--- lib/features/pages/chat_with_dev/ui/page/chat_with_dev.dart
+++ lib/features/pages/chat_with_dev/ui/page/chat_with_dev.dart
@@ -1,43 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tadamon/core/config/const/sensei_const.dart';
+import 'package:tadamon/core/services/url_services/url_services.dart';
+import 'package:tadamon/core/widgets/app_toast/app_toast.dart';
 import 'package:tadamon/core/widgets/text_filed_component/text_filed_component.dart';
 import 'package:tadamon/features/pages/chat_with_dev/ui/widget/chat_bubble.dart';
 import 'package:tadamon/features/pages/chat_with_dev/ui/widget/chat_dev_app_bar.dart';
 import 'package:tadamon/generated/l10n.dart';
 
 class ChatWithDev extends StatelessWidget {
-  const ChatWithDev({super.key});
+  ChatWithDev({super.key});
+
+  final TextEditingController _controller = TextEditingController();
+
+  void dispose() {
+    _controller.dispose();
+  }
+
+  Future<void> sendMessage() async {
+    try {
+      final String message = _controller.text;
+      if (message.isNotEmpty) {
+        await UrlRunServices.sendEmail(
+          toEmail: 'mostafa438886@fci.bu.edu.eg',
+          subject: 'مرحبا، MR: Mostafa Sensei',
+          body: message,
+        );
+        _controller.clear();
+      }
+    } catch (e) {
+      AppToast.showErrorToast(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final List<Map<String, dynamic>> messages = [
-  {
-    'text': 'شكرًا لاستخدامك هذا التطبيق! 🎉',
-    'isSentByMe': false,
-  },
-  {
-    'text': 'إذا عجبك التطبيق، يسعدنا دعمك عشان نقدر نستمر في تطويره! ❤️',
-    'isSentByMe': false,
-  },
-  {
-    'text' : 'للاستفسار، يمكنك التواصل معنا على الواتساب 👇',
-    'isSentByMe': true,
-  },
-  {
-    'text': 'تقدر تتابعنا وتدعمنا على وسائل التواصل 👇',
-    'isSentByMe': false,
-  },
-  {
-    'text': '📌 تابعنا على لمزيد من التحديثات والدعم 🙌',
-    'isSentByMe': false,
-  },
-  {
-    'text': 'وشارك التطبيق مع أصدقائك! 📲',
-    'isSentByMe': false,
-    'isShareApp': true,
-  },
-];
+      {
+        'text': 'شكرًا لاستخدامك هذا التطبيق! 🎉',
+        'isSentByMe': false,
+      },
+      {
+        'text': 'إذا عجبك التطبيق، يسعدنا دعمك عشان نقدر نستمر في تطويره! ❤️',
+        'isSentByMe': false,
+      },
+      {
+        'text': 'للاستفسار، يمكنك التواصل معنا على الواتساب 👇',
+        'isSentByMe': true,
+      },
+      {
+        'text': 'تقدر تتابعنا وتدعمنا على وسائل التواصل 👇',
+        'isSentByMe': false,
+      },
+      {
+        'text': '📌 تابعنا على لمزيد من التحديثات والدعم 🙌',
+        'isSentByMe': false,
+      },
+      {
+        'text': 'وشارك التطبيق مع أصدقائك! 📲',
+        'isSentByMe': false,
+        'isShareApp': true,
+      },
+    ];
 
     final now = DateTime.now();
     return Scaffold(
@@ -72,24 +96,18 @@ class ChatWithDev extends StatelessWidget {
               padding: const EdgeInsets.only(
                 left: SenseiConst.padding,
                 right: SenseiConst.padding,
-                bottom: SenseiConst.padding,
-                top: 5,
+                bottom: 5.0,
+                top: 5.0,
               ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextFieldComponent(
-                      controller: TextEditingController(),
-                      icon: Icons.message_outlined,
-                      useOutBorderRadius: true,
-                      hint: '...اكتب رسالة',
-                      suffixIcon: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.send_outlined)),
-                    ),
-                  ),
-                  const SizedBox(width: SenseiConst.margin),
-                ],
+              child: TextFieldComponent(
+                controller: _controller,
+                icon: Icons.message_outlined,
+                useOutBorderRadius: true,
+                hint: '...اكتب رسالة',
+                suffixIcon: IconButton(
+                  onPressed: () => sendMessage(),
+                  icon: const Icon(Icons.send_outlined),
+                ),
               ),
             ),
           ],
